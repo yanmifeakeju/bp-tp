e8dfc5a311df96523178b4624e5efe62427cd2f5
Oluwayanmife Akeju <yanmifeakeju@gmail.com>
2022-02-05 19:46:28 +0100

    Handle test scenario for empty payload

--- features/steps/steps.js
+++ features/steps/steps.js
@@ -1,32 +1,42 @@
 import { When, Then } from '@cucumber/cucumber';
+import superagent from 'superagent';
+import assert from 'assert';
 
-When(/the client creates a POST request to \/users/, function () {
+When(/the client creates a POST request to \/users/, async function () {
   // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+  this.client = superagent('POST', `localhost:${process.env.PORT}/users`);
 });
 
-When('attaches a generic empty payload', function () {
+When('attaches a generic empty payload', async function () {
   // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+  return undefined;
 });
 
-When('sends the request', function () {
+When('sends the request', async function () {
   // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+  try {
+    this.response = await this.client;
+  } catch (error) {
+    this.response = error.response;
+  }
 });
 
 Then('the API should respond with a {int} HTTP', function (statusCode) {
-  // Then('the API should respond with a {float} HTTP', function (float) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+  assert.equal(this.response.statusCode, statusCode);
 });
 
 Then('the payload of the response should be a JSON object', function () {
   // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+  assert(this.response.headers['content-type'].includes('application/json'));
+
+  try {
+    this.body = JSON.parse(this.response.text);
+  } catch (error) {
+    throw new Error('Response not valid json');
+  }
 });
 
-Then('contains a message property which says {string}', function (string) {
+Then('contains a message property which says {string}', function (message) {
   // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+  assert.equal(this.body.message, message);
 });
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
+
+app.post('/users', (req, res) => {
+  if (Object.keys(req.body).length === 0) {
+    res.status(400);
+    res.json({ message: 'Payload should not be empty' });
+    return;
+  }
+  res.send();
+});
 
 app.get('/', (req, res) => {
   res.send('Set up');
 });
 
-app.listen(3000, process.stdout.write('App initialization'));
+app.listen(process.env.PORT, process.stdout.write(`App initialized on ${process.env.PORT}`));
